b981d7762f7a7623c4297fa2be949c8e45ca222d
DirkVDerMeulen <dirk-vandermeulen@hotmail.com>
2021-05-14 12:15:09 +0200

    Adjustments to setup.py and version initiation

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
+import os
+
 from setuptools import setup
 
 NAME = 'TS_AutoML'
@@ -10,6 +13,23 @@ EMAIL = 'dirk-vandermeulen@hotmail.com'
 AUTHOR = 'Dirk van der Meulen'
 REQUIRES_PYTHON = '>=3.6.0'
 VERSION = '0.0.2'
+
+# Necessary requirements for package
+REQUIRED = [
+    'numpy',
+    'pandas',
+    'sklearn'
+]
+
+# Load the package's __version__.py module as a dictionary.
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
 
 # Where the magic happens
 setup(
@@ -21,5 +41,7 @@ setup(
     author_email=EMAIL,
     license='unlicense',
     packages=['TS_AutoML'],
+    install_requires=REQUIRED,
+    include_package_data=True,
     zip_safe=False
 )
