53c52496175d714e24f91889375158b4c7f6dc03
DirkVDerMeulen <dirk-vandermeulen@hotmail.com>
2021-05-26 17:01:29 +0200

    Added rolling forward as function to RF

--- TS_AutoML/TS_AutoML/regressors.py
+++ TS_AutoML/TS_AutoML/regressors.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
 from sklearn.ensemble import RandomForestRegressor
-from TS_AutoML.functions import ParameterSearch
+
+from TS_AutoML.functions import (
+    ParameterSearch,
+    RollingForecast
+)
 
 from typing import (
     Dict,
     AnyStr,
     Tuple,
+    List
 )
 
 
@@ -45,3 +50,23 @@ class RandomForest:
         best_fit, all_results = search.optimize_parameters()
 
         return best_fit, all_results
+
+    @staticmethod
+    def rolling_forecast(
+            df: pd.DataFrame,
+            groupby: List,
+            time_column: AnyStr,
+            target_column: AnyStr,
+            retrain_frequency: int = 1,
+            prediction_start_date: int = 0,
+            prediction_end_date: int = 0,
+            prediction_lag: int = 1,
+            **model_params) -> Tuple:
+        rolling_forward_predictor = RollingForecast(predictor=RandomForest, df=df, groupby=groupby,
+                                                    time_col=time_column, target_value=target_column,
+                                                    retrain_frequency=retrain_frequency,
+                                                    prediction_start_date=prediction_start_date,
+                                                    prediction_end_date=prediction_end_date,
+                                                    **model_params)
+        prediction_results, accuracy = rolling_forward_predictor.predict()
+        return prediction_results, accuracy
